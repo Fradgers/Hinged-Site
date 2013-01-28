65dd6b3a2b982913f926bbe5275325728c2cf0b3
fradgers <fradgers@gmail.com>
2013-01-28 23:02:14 +0000

    Added new pictures to slideshow on main pages.

--- reviews.php
+++ reviews.php
@@ -10,12 +10,17 @@ $page->page_id = 'reviews';
 
 $page->slides = array(
 	'resources/images/dance_photos_3.jpg',
+	'resources/images/Pike and a half.jpg',
 	'resources/images/IMG_8438.jpg',
+	'resources/images/Pike chair.jpg',
 	'resources/images/sepia1.jpg',
 	'resources/images/IMG_8444.jpg',
+	'resources/images/Home end.jpg',
 	'resources/images/tairabw_50_edit.jpg',
+	'resources/images/E & B hold.jpg',
 	'resources/images/IMG_8446.jpg',
 	'resources/images/sepia2.jpg',
+	'resources/images/Swimming Duet.jpg',
 	'resources/images/IMG_8457.jpg',
 	'resources/images/TairaPose11.jpg',
 	'resources/images/IMG_8469.jpg',
